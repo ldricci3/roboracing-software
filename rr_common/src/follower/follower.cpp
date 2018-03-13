0daaa7702936e19721adfbc707f62ff0673bdb0e
holben888 <holben888@gmail.com>
2018-03-12 20:33:48 -0400

    follower launch file moved, map callback build out; need pointcloud

--- rr_common/src/follower/follower.cpp
+++ rr_common/src/follower/follower.cpp
@@ -7,11 +7,11 @@ void mapCallback(const sensor_msgs::PointCloud2ConstPtr& map) {
     pcl_conversions::toPCL(*map, pcl_pc2);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud <pcl::PointXYZ>);
     pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
+    rr_platform::speedPtr speedMSG(new rr_platform::speed);
+    rr_platform::steeringPtr steerMSG(new rr_platform::steering);
     if (cloud->empty()) {
         ROS_WARN("environment map pointcloud is empty");
-        rr_platform::speedPtr speedMSG(new rr_platform::speed);
-        rr_platform::steeringPtr steerMSG(new rr_platform::steering);
-        speedMSG->speed = MAX_SPEED / 4; //proceed with caution; E-Kill if necessary
+        speedMSG->speed = 0;
         steerMSG->angle = 0;
         steerMSG->header.stamp = ros::Time::now();
         speedMSG->header.stamp = ros::Time::now();
@@ -19,40 +19,54 @@ void mapCallback(const sensor_msgs::PointCloud2ConstPtr& map) {
         steer_pub.publish(steerMSG);
         return;
     }
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
-    pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
-    kdtree.setInputCloud(cloud);
-}
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud (cloud);
+    pass.setFilterFieldName ("x");
+    pass.setFilterLimits (MIN_X, MAX_X);
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (GROUND_THRESHOLD, MAX_Z);
+    pass.filter (*cloud_filtered); //creates filtered cloud excluding objects too far left or right, as well as those on the ground
 
-void parseFloatArrayStr(string &arrayAsString, vector<float> &floats) {
-    char s[arrayAsString.size()];
-    strcpy(s, arrayAsString.c_str());
-    char* token = strtok(s, " ,");
-    while(token != NULL) {
-        floats.push_back(stof(string(token)));
-        token = strtok(NULL, " ,");
+    float avgY = 0.0f;
+
+    for (pcl::PointXYZ point : cloud_filtered->points) {
+        avgY += point.y;
     }
+    avgY = avgY / cloud_filtered->points.size(); //computes the average distance away points are from the sensor directly forward
+
+    steerMSG->header.stamp = ros::Time::now();
+    speedMSG->header.stamp = ros::Time::now();
+    if (avgY > 0 && avgY <= GOAL_Y)
+        speedMSG->speed = -(FOLLOWER_SPEED); //when object is too close, move backward
+    else if (avgY < MAX_Y && avgY >= GOAL_Y)
+        speedMSG->speed = FOLLOWER_SPEED; //when object is too far, move forward
+    else
+        speedMSG->speed = 0; //when the object exceeds MAX_Y or another anomali occurs
+
+    steerMSG->angle = 0;
+    speed_pub.publish(speedMSG);
+    steer_pub.publish(steerMSG);
 }
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "follower");
-    string obstacleCloudTopic;
+    string cloudTopic;
 
     ros::NodeHandle nh;
     ros::NodeHandle nhp("~");
 
     nhp.param("MIN_X", MIN_X, -0.2f);
     nhp.param("MAX_X", MAX_X, 0.2f);
-    nhp.param("MIN_Y", MIN_Y, 0.1f);
-    nhp.param("MAX_Y", MAX_Y, 0.5f);
-    nhp.param("MAX_Z", MAX_Z, 1.0f);
-    nhp.param("GOAL_Z", GOAL_Z, 0.6f);
-    nhp.param("Z_SCALE", Z_SCALE, 1.0f);
-    nhp.param("X_SCALE", X_SCALE, 5.0f);
-    nhp.param("MAX_SPEED", MAX_SPEED, 0.5f);
-    nhp.param("INPUT_CLOUD_TOPIC", obstacleCloudTopic, string("/map"));
+    nhp.param("GOAL_Y", GOAL_Y, 0.5f);
+    nhp.param("MAX_Y", MAX_Y, 3.0f);
+    nhp.param("MAX_Z", MAX_Z, 5.0f);
+    nhp.param("GROUND_THRESHOLD", GROUND_THRESHOLD, 0.1f);
+    nhp.param("FOLLOWER_SPEED", FOLLOWER_SPEED, 1.0f);
+    nhp.param("INPUT_CLOUD_TOPIC", cloudTopic, string("/map"));
 
-    auto map_sub = nh.subscribe(obstacleCloudTopic, 1, mapCallback);
+    auto map_sub = nh.subscribe(cloudTopic, 1, mapCallback);
     speed_pub = nh.advertise<rr_platform::speed>("plan/speed", 1);
     steer_pub = nh.advertise<rr_platform::steering>("plan/steering", 1);
 
--- rr_common/src/follower/follower.h
+++ rr_common/src/follower/follower.h
@@ -2,32 +2,22 @@
 #define RR_COMMON_FOLLOWER_H
 
 #include <ros/ros.h>
-#include <cmath>
-#include <cstring>
-#include <string>
-#include <random>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
 #include <pcl/PCLPointCloud2.h>
-#include <pcl/conversions.h>
-#include <pcl_ros/transforms.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/passthrough.h>
+#include <string>
 #include <rr_platform/speed.h>
 #include <rr_platform/steering.h>
-#include <nav_msgs/Path.h>
-#include <geometry_msgs/PoseStamped.h>
 #include "flann/flann.hpp"
 
-float MIN_Y;
 float MAX_Y;
+float GOAL_Y;
 float MIN_X;
 float MAX_X;
 float MAX_Z;
-float GOAL_Z;
-float Z_SCALE;
-float X_SCALE;
-
-float MAX_SPEED;
+float GROUND_THRESHOLD;
+float FOLLOWER_SPEED;
 
 ros::Publisher speed_pub, steer_pub;
 
